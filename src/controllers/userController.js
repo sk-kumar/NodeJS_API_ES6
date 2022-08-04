c49291329fdb01466eb8bfd892f96ceb06df52f4
SKumar <sk.kumar561@gmail.com>
2022-08-04 18:37:10 +0530

    first commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,12 +3,15 @@ import userModel from "../models/userModel.js";
 const create = async (req, res) => {
     try {
         console.log(req.body);
-        const data = await userModel.create(req.body)
+        // const data = await userModel.create(req.body);
+        const data = userModel(req.body);
+        await data.save();
         res.status(201).json(data);
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: error.message });
     }
 }
+
 
 export default create;
