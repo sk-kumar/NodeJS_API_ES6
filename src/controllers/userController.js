3b47650e56e7a857f9b63bf0db92b57e795e705c
SKumar <sk.kumar561@gmail.com>
2022-08-04 19:28:26 +0530

    first commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,27 @@
 import userModel from "../models/userModel.js";
 
-const create = async (req, res) => {
-    try {
-        console.log(req.body);
-        // const data = await userModel.create(req.body);
-        const data = userModel(req.body);
-        await data.save();
-        res.status(201).json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
+class userController {
+    static userRegistration = async (req, res) => {
+        try {
+            console.log(req.body);
+            const data = userModel(req.body);
+            await data.save();
+            res.status(201).json(data);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static userData = async (req, res) => {
+        try {
+            const data = await userModel.find();
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
 }
 
+export default userController;
 
-export default create;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import create from '../controllers/userController.js';
+import userController from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.post('/register', create);
+router.post('/register',userController.userRegistration);
+router.get('/user', userController.userData);
 
 export default router;
